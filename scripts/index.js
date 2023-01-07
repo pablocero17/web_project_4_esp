60f792071a46cfe84ffe6c26782f0c686b009594
pablocero17 <pablocero17@gmail.com>
2023-01-06 21:46:56 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,7 @@ const closeEditProfileButton = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
 const form = document.querySelector(".form__profile-edit");
 const nameInput = document.querySelector(".form__input_type_name");
-const descriptionInput = document.querySelector(
-  ".form__input_type_description"
-);
+const descriptionInput = document.querySelector(".form__input_type_description");
 const profileName = document.querySelector(".profile__name-text");
 const profileDescription = document.querySelector(".profile__description");
 
